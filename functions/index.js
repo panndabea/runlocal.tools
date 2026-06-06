dbdbb9fca80df86df5f1bed1800dbd4fb0b0fd35
Omio <mahatma.gates@web.de>
2026-06-06 15:08:07 +0200

    Preserve verify URL parameters

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
 export function onRequest(context) {
+  const url = new URL(context.request.url);
   const host = context.request.headers.get("host");
 
-  if (host === "verify.runlocal.tools") {
+  if (host === "verify.runlocal.tools" && url.pathname === "/") {
     return Response.redirect(
-      "https://verify.runlocal.tools/verify.html",
+      `https://verify.runlocal.tools/verify.html${url.search}`,
       302
     );
   }
